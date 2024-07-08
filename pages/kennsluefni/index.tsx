d1514eb2c3c1a902619375e796111d648eb62f2f
Doddi <doddiskula@gmail.com>
2024-07-08 22:30:21 +0000

    fix errors

--- pages/kennsluefni/index.tsx
+++ pages/kennsluefni/index.tsx
@@ -15,7 +15,7 @@ export default function KennsluefniPage({ courses }) {
       <Container>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {courses.map((course, index) => (
-            <CourseCard key={index} course={course} colorIndex={index + 1} />
+            <CourseCard key={index} course={course} />
           ))}
         </div>
       </Container>
